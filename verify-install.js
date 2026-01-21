9ee5447c7e0595cdfea36dc71b88400edaf4074d
JC-Morales03 <juan_morales8@usmp.pe>
2026-01-20 19:31:08 -0500

    Quitar utilidades

--- verify-install.js
+++
@@ -1,8 +0,0 @@
-try {
-  require('cookie-parser');
-  console.log('✅ cookie-parser está instalado correctamente.');
-} catch (e) {
-  console.error('❌ ERROR: cookie-parser NO se encuentra.');
-  console.error(e.message);
-  process.exit(1);
-}
